3d106bbd44b538cb06aad48cd0ddb9cf40b69f76
Gundam073 <mr.bill.cheung@gmail.com>
2023-02-25 17:46:56 +0000

    Update to  use Honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -3,6 +3,12 @@ from flask import request
 from flask_cors import CORS, cross_origin
 import os
 
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from services.home_activities import *
 from services.notifications_activities import *
 from services.user_activities import *
@@ -13,6 +19,18 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+# Initialize tracing and an exporter that can send data to Honeycomb
+provider = TracerProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
+
+# Initialize automatic instrumentation with Flask
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 app = Flask(__name__)
 frontend = os.getenv('FRONTEND_URL')

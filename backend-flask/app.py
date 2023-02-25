3d0904e396d456811b5272a18d8bb6134a5bf471
Gundam073 <mr.bill.cheung@gmail.com>
2023-02-25 18:36:08 +0000

    partial add tracer

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -10,6 +10,7 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 # Services -------------
 from services.home_activities import *
 from services.notifications_activities import *
@@ -27,6 +28,9 @@ from services.show_activity import *
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
+simpleprocessor = SimpleSpanProcessor(ConsoleSpanExporter())
+provider.add_span_processor(simpleprocessor)
+
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
